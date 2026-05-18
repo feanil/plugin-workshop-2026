378612cb26610c1f0c17591b86176c855464e396
Feanil Patel <feanil@axim.org>
2026-05-18 12:06:44 -0400

    doc: Update the password.

--- connect.sh
+++ connect.sh
@@ -1,3 +1,3 @@
 ip=${1}
-echo "The password is: plugin-workshop-2026"
+echo "The password is: workshop26
 ssh -o TCPKeepAlive=yes -o ServerAliveInterval=120 $(for i in 8000 8001 8002 7700 2025 1984 1993 1994 1995 1996 1997 1998 1999 2000 2001 2002; do echo -L ${i}:localhost:${i}; done) workshop_dev@${ip}
